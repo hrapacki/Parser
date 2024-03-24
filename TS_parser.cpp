0e7015124921db789228f10a52ade818fbc6e876
vombat <coolmail@gmail.com>
2024-03-24 17:37:56 +0100

    Changes to be committed:

--- TS_parser.cpp
+++ TS_parser.cpp
@@ -1,20 +1,24 @@
 #include "tsCommon.h"
 #include "tsTransportStream.h"
-
+#include <fstream>
+#include <iostream>
+using namespace std;
 //=============================================================================================================================================================================
 
 int main(int argc, char *argv[ ], char *envp[ ])
 {
   // TODO - open file
   // TODO - check if file if opened
-
+  cout << "siema";
   xTS_PacketHeader    TS_PacketHeader;
 
   int32_t TS_PacketId = 0;
   while(/*not eof*/ 0)
   {
+    cout << "siema";
     // TODO - read from file
-
+    ofstream myfile;
+    myfile.open("example_new.ts");
     TS_PacketHeader.Reset();
     TS_PacketHeader.Parse(/*TS_PacketBuffer*/ nullptr);
 
--- tsTransportStream.cpp
+++ tsTransportStream.cpp
@@ -1,5 +1,7 @@
 #include "tsTransportStream.h"
 #include "tsCommon.h"
+#include <iostream>
+using namespace std;
 //=============================================================================================================================================================================
 // xTS_PacketHeader
 //=============================================================================================================================================================================
@@ -26,23 +28,39 @@ void xTS_PacketHeader::Reset()
  */
 int32_t xTS_PacketHeader::Parse(const uint8_t* Input)
 {
-  uint16_t zero=8191;
   uint32_t* HP = (uint32_t*)Input;
   uint32_t Head = xSwapBytes32(*HP);
   m_CC = m_CC|Head;
   Head>>4;
+  m_CC&15;
   m_AFC = m_AFC|Head;
   Head>>2;
+  m_AFC&3;
   m_TSC = m_TSC|Head;
   Head>>2;
-  
+  m_TSC&3;
+  m_PID = m_PID|Head;
+  Head>>13;
+  m_PID|8191;
+  m_T = m_T|Head;
+  Head>>1;
+  m_T&1;
+  m_S = m_S|Head;
+  Head>>1;
+  m_S&1;
+  m_E = m_E|Head;
+  Head>>1;
+  m_E&1;
+  m_SB = m_SB|Head;
+  Head>>8;
   return NOT_VALID;
 }
 
 /// @brief Print all TS packet header fields
 void xTS_PacketHeader::Print() const
 {
-  //print sth
-}
+ cout << "SB: " << m_SB << " E: " << m_E << " S: " << m_S << " T: " << m_T;
+ cout << " PID: " << m_PID << " TSC: " << m_TSC << " AFC: " << m_AFC << " CC: " << m_CC << endl;
+} 
 
 //=============================================================================================================================================================================
