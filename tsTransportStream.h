e588c610b4842da7c6f7e19e4b3a60fd0f8df9d1
hrapacki <151746745+hrapacki@users.noreply.github.com>
2024-06-02 00:18:16 +0200

    Update tsTransportStream.h
    Finished tsTransportStream.h

--- tsTransportStream.h
+++ tsTransportStream.h
@@ -3,33 +3,6 @@
 #include <string>
 
 /*
-MPEG-TS packet:
-`        3                   2                   1                   0  `
-`      1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0  `
-`     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ `
-`   0 |                             Header                            | `
-`     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ `
-`   4 |                  Adaptation field + Payload                   | `
-`     |                                                               | `
-` 184 |                                                               | `
-`     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ `
-
-
-MPEG-TS packet header:
-`        3                   2                   1                   0  `
-`      1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0  `
-`     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ `
-`   0 |       SB      |E|S|T|           PID           |TSC|AFC|   CC  | `
-`     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ `
-
-Sync byte                    (SB ) :  8 bits
-Transport error indicator    (E  ) :  1 bit
-Payload unit start indicator (S  ) :  1 bit
-Transport priority           (T  ) :  1 bit
-Packet Identifier            (PID) : 13 bits
-Transport scrambling control (TSC) :  2 bits
-Adaptation field control     (AFC) :  2 bits
-Continuity counter           (CC ) :  4 bits
 */
 
 
@@ -67,14 +40,14 @@ public:
   };
 
 protected:
-  uint8_t  m_SB;
-  uint8_t m_E;
-  uint8_t  m_S;
-  uint8_t  m_T;
+  uint16_t  m_SB;
+  uint16_t m_E;
+  uint16_t  m_S;
+  uint16_t  m_T;
   uint16_t m_PID;
-  uint8_t  m_TSC;
-  uint8_t  m_AFC;
-  uint8_t  m_CC;
+  uint16_t  m_TSC;
+  uint16_t  m_AFC;
+  uint16_t  m_CC;
 
 public:
   void     Reset();
@@ -82,23 +55,141 @@ public:
   void     Print() const;
 
 public:
-  uint8_t  getSyncByte() const { return m_SB; }  
-  uint8_t  get_E() const { return m_E; }  
-  uint8_t  get_S() const { return m_S; }  
-  uint8_t  get_T() const { return m_T; }  
-  uint16_t get_PID() const { return m_PID; }  
-  uint8_t  get_TSC() const { return m_TSC; }  
-  uint8_t  get_AFC() const { return m_AFC; }  
-  uint8_t  get_CC() const { return m_CC; }  
-  
+  uint16_t  getSyncByte() const { return m_SB; }
+  uint16_t  get_E() const { return m_E; }
+  uint16_t  get_S() const { return m_S; }
+  uint16_t  get_T() const { return m_T; }
+  uint16_t get_PID() const { return m_PID; }
+  uint16_t  get_TSC() const { return m_TSC; }
+  uint16_t  get_AFC() const { return m_AFC; }
+  uint16_t  get_CC() const { return m_CC; }
+
 public:
   //TODO - derrived informations
   bool     hasAdaptationField() const {
     return (m_AFC==2||m_AFC==3);
    }
-  bool     hasPayload        () const { 
+  bool     hasPayload        () const {
     return !(m_AFC==2||m_AFC==0);
    }
 };
 
+class xTS_AdaptationField
+{
+protected:
+//setup
+uint16_t m_AdaptationFieldControl;
+uint16_t DC;
+uint16_t RA;
+uint16_t SP;
+uint16_t PR;
+uint16_t OR;
+uint16_t SF;
+uint16_t TP;
+uint16_t EX;
+uint16_t m_AdaptationFieldLength;
+//optional fields - PCR
+public:
+void Reset();
+int32_t Parse(const uint8_t* PacketBuffer, uint8_t AdaptationFieldControl);
+void Print() const;
+public:
+//mandatory fields
+uint16_t getAdaptationFieldLength () const { return m_AdaptationFieldLength ; }
+
+uint8_t getDC () const { return DC ; }
+uint8_t getRA () const { return RA ; }
+uint8_t getSP () const { return SP ; }
+uint8_t getPR () const { return PR ; }
+uint8_t getOR () const { return OR ; }
+uint8_t getSF () const { return SF ; }
+uint8_t getTP () const { return TP ; }
+uint8_t getEX () const { return EX ; }
+//derived values
+//uint32_t getNumBytes () const { }
+};
 //=============================================================================================================================================================================
+class xPES_PacketHeader
+{
+public:
+enum eStreamId : uint8_t
+{
+eStreamId_program_stream_map = 0xBC,
+eStreamId_padding_stream = 0xBE,
+eStreamId_private_stream_2 = 0xBF,
+eStreamId_ECM = 0xF0,
+eStreamId_EMM = 0xF1,
+eStreamId_program_stream_directory = 0xFF,
+eStreamId_DSMCC_stream = 0xF2,
+eStreamId_ITUT_H222_1_type_E = 0xF8,
+};
+protected:
+//PES packet header
+uint32_t m_PacketStartCodePrefix;
+uint8_t m_StreamId;
+uint16_t m_PacketLength;
+public:
+void Reset();
+int32_t Parse(const uint8_t* Input);
+void Print() const;
+public:
+    uint16_t m_HeaderLength;
+
+//PES packet header
+uint16_t GetHeaderLength() const {return m_HeaderLength;}
+uint32_t getPacketStartCodePrefix() const { return m_PacketStartCodePrefix; }
+uint8_t getStreamId () const { return m_StreamId; }
+uint16_t getPacketLength () const { return m_PacketLength; }
+};
+
+class xPES_Assembler
+{
+public:
+enum class eResult : int32_t
+{
+UnexpectedPID = 1,
+StreamPacketLost ,
+AssemblingStarted ,
+AssemblingContinue,
+AssemblingFinished,
+};
+protected:
+//setup
+int16_t Datalen;
+int32_t m_PID;
+//buffer
+
+uint32_t m_BufferSize;
+uint32_t m_DataOffset;
+//operation
+int8_t m_LastContinuityCounter;
+bool m_Started;
+
+public:
+    uint8_t* m_Buffer;
+    xPES_PacketHeader m_PESH;
+void Reset();
+xPES_Assembler ();
+~xPES_Assembler();
+void Init (int32_t PID);
+
+eResult AbsorbPacket(const uint8_t* TransportStreamPacket, const xTS_PacketHeader* PacketHeader, const xTS_AdaptationField* AdaptationField);
+uint16_t CalcDataLen(){
+Datalen= getNumPacketBytes()-m_PESH.GetHeaderLength();
+return Datalen;
+}
+void PrintPESH ()  { m_PESH.Print(); }
+uint8_t* getPacket () { return m_Buffer; }
+int32_t getNumPacketBytes() const {
+    if(m_DataOffset >= m_PESH.getPacketLength()- m_PESH.m_HeaderLength){
+            return m_DataOffset+m_PESH.GetHeaderLength();}
+    else{
+       return m_DataOffset;
+    }
+     }
+protected:
+void xBufferReset ();
+void xBufferAppend(const uint8_t* Data, int32_t Size);
+};
+
+
